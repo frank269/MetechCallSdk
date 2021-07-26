47fb2dd82dce894753dfc3f8a975d67ae44e4e23
Tiendoan <tien2609@gmail.com>
2021-07-26 14:05:43 +0700

    add new framework

---
+++ Sources/MetechCallSdk.xcframework/ios-arm64/MetechCallSdk.framework/Headers/MetechCallSdk.h
@@ -0,0 +1,18 @@
+//
+//  MetechCallSdk.h
+//  MetechCallSdk
+//
+//  Created by Tien Doan on 5/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MetechCallSdk.
+FOUNDATION_EXPORT double MetechCallSdkVersionNumber;
+
+//! Project version string for MetechCallSdk.
+FOUNDATION_EXPORT const unsigned char MetechCallSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetechCallSdk/PublicHeader.h>
+
+
